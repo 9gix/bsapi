4c096e53c37119b323e329e140904817958cdc8d
Eugene <yeo.eugene.oey@gmail.com>
2014-02-23 22:23:27 +0800

    Activate admin page

--- comm/admin.py
+++ comm/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from comm.models import Conversation, ConversationMessage
 
-# Register your models here.
+admin.site.register(Conversation)
+admin.site.register(ConversationMessage)
