938b5380b9842265d142fdae79b9db7c0ecbe903
Eugene <yeo.eugene.oey@gmail.com>
2014-05-31 12:16:10 +0800

    update requirements

--- requirements.py
+++ requirements.py
@@ -7,8 +7,8 @@ django-autoslug>=1.7.2,<1.8
 
 pyisbn>=1.0.0,<1.1.0
 
-# djangorestframework>=2.3.12,<2.4.0
--e git+git@github.com:9gix/django-rest-framework.git@master#egg=djangorestframework
+djangorestframework>=2.3.13,<2.4.0
+#-e git+git@github.com:9gix/django-rest-framework.git@unstable-merged#egg=djangorestframework
 
 django-oauth-toolkit>=0.7.1,<0.8
 
@@ -18,7 +18,7 @@ markdown>=2.4,<2.5
 
 django-filter>=0.7,<0.8
 
-django-debug-toolbar>=1.0.1,<1.1
+django-debug-toolbar>=1.2.1,<1.3
 
 gdata>=2.0.18,<2.1
 
@@ -28,6 +28,10 @@ ipdb>=0.8,<0.9
 
 bpython>=0.12,<1
 
+ipython>=2.1.0,<2.2
+
+pyzmq>=14.3.0,<14.4
+
 celery>=3.1.9,<3.2
 
 elasticsearch>=1.0.0,<1.1
@@ -35,3 +39,5 @@ elasticsearch>=1.0.0,<1.1
 pyelasticsearch>=0.6.1,<0.7
 
 django-haystack>=2.1.0,<2.2
+
+python-dateutil>=2.2,<2.3
