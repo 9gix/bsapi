830d2afb75e9432b8d44bcd338e49514924afa63
Eugene <yeo.eugene.oey@gmail.com>
2014-06-07 03:50:29 +0800

    Add model doc for BookPicture

--- ownership/models.py
+++ ownership/models.py
@@ -14,6 +14,8 @@ class Book(models.Model):
         return "%s (owner: %s)" % (self.book_profile, self.owner)
 
 class BookPicture(models.Model):
+    """This model contains the physical book photo, it is used to
+    display the condition of the book."""
     book = models.ForeignKey('ownership.Book')
     image = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
