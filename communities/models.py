c966f4f46e52d6ba52dd2dc633288213f8683165
Eugene <yeo.eugene.oey@gmail.com>
2014-06-08 13:49:18 +0800

    Community moderators

--- communities/models.py
+++ communities/models.py
@@ -4,7 +4,15 @@ from django.conf import settings
 class Community(models.Model):
     name = models.CharField(max_length=40, unique=True)
     logo = models.ImageField(upload_to='community-logo', blank=True)
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL,
+            through='communities.Membership')
 
     def __str__(self):
         return self.name
+
+class Membership(models.Model):
+    community = models.ForeignKey('communities.Community')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    is_moderator = models.BooleanField(default=False)
+
