70fc9f17e74d2809f59369d95ac5da77441e25ea
Yan Tianqi <yantq123@hotmail.com>
2014-06-26 17:57:14 +0800

    third commit, models are more or less ready. next is to move on to url/serializer/view

--- reputation/models.py
+++ reputation/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.conf import settings
 
 ###this model contains the record of user's reputation
 
 class Reputation(models.Model):
-    userProfile = models.OneToOneField(accounts.UserProfile)
     record = models.IntegerField(default=10)
-###userProfile linked to accounts.userProfile, initial total reputation amount set to be 10
+### initial total reputation amount set to be 10
 
     def increase(self, amount=1):
         record += amount
