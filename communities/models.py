8dbcbbdc61bc3a6e6fc0c27195a71424ef2f15d3
Yan Tianqi <yantq123@hotmail.com>
2014-06-30 10:46:12 +0800

    debug the previous one, found inconsistent use of tab and space

--- communities/models.py
+++ communities/models.py
@@ -13,7 +13,7 @@ class Community(models.Model):
 class Membership(models.Model):
     community = models.ForeignKey('communities.Community')
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-	reputation = models.OneToOneField('reputation.Reputation')
+    reputation = models.OneToOneField('reputation.Reputation')
 
     is_moderator = models.BooleanField(default=False)
 
