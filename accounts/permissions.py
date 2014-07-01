13616189c513cc0ade0b42f435852e44d4646072
Eugene <yeo.eugene.oey@gmail.com>
2014-07-02 02:28:02 +0800

    rename permission class

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
 
-class IsAdminOrIsSelf(permissions.BasePermission):
+class IsAdminOrSelf(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or request.user == obj
