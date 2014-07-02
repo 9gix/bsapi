4b818073cc65faff0aa2b73a0db46b139c920148
Eugene <yeo.eugene.oey@gmail.com>
2014-07-03 03:42:19 +0800

    Remove Reputation Views and serve the reputation in the membership

--- communities/serializers.py
+++ communities/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from communities.models import Community
+from communities.models import Community, Membership
 
 class CommunitySerializer(serializers.ModelSerializer):
     members = serializers.RelatedField(many=True)
 
     class Meta:
         model = Community
+
+class MembershipSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Membership
