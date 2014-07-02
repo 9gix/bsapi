6b350b061926ba40b9bed211da97bb779225ee4f
Eugene <yeo.eugene.oey@gmail.com>
2014-07-03 03:16:18 +0800

    Remove Unnecessary import

--- reputation/models.py
+++ reputation/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.conf import settings
 
 
 class Reputation(models.Model):
--- reputation/views.py
+++ reputation/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
-from rest_framework import views
-from rest_framework import status
-from rest_framework import response
-from rest_framework import generics
-from rest_framework.views import APIView
 
 from reputation.models import Reputation
 from reputation.serializers import ReputationSerializer
