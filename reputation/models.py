5d90e2e12ae74f9abaf89bdc17a98ec0236bcf45
Eugene <yeo.eugene.oey@gmail.com>
2014-07-03 02:56:07 +0800

    Remove comments for an obvious code

--- reputation/models.py
+++ reputation/models.py
@@ -3,16 +3,14 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.conf import settings
 
-###this model contains the record of user's reputation
 
 class Reputation(models.Model):
-    value = models.IntegerField(default=10)
-### initial total reputation amount set to be 10
+    """this model contains the record of user's reputation"""
+
+    value = models.IntegerField(default=0)
 
     def increase(self, amount=1):
-        value += amount
-###function to increase user's reputation, default set to be 1
+        self.value += amount
 
     def decrease(self, amount=1):
-        value -= amount
-###function to increase user's reputation, default set to be 1
+        self.value -= amount
