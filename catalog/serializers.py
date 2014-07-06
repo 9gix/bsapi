82deade809b6980d4ba6632e39949d01b9a73802
Eugene <yeo.eugene.oey@gmail.com>
2014-07-06 13:10:22 +0800

    Fix m2m, error when required=false on m2m

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -8,12 +8,10 @@ class BookSerializer(serializers.ModelSerializer):
             view_name="book-detail",
             lookup_field='isbn13')
     isbn = serializers.CharField(source='isbn13')
-    authors = serializers.SlugRelatedField(many=True, slug_field='name',
-            required=False)
+    authors = serializers.SlugRelatedField(many=True, slug_field='name')
     publisher = serializers.SlugRelatedField(slug_field='name',
             required=False)
-    categories = serializers.SlugRelatedField(many=True, slug_field='name',
-            required=False)
+    categories = serializers.SlugRelatedField(many=True, slug_field='name')
     owners = serializers.RelatedField(many=True)
 
     class Meta:
