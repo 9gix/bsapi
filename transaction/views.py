2b1318b9195f0195ad47e12c7af17b99edf23b1e
Yan Tianqi <yantq123@hotmail.com>
2014-07-06 09:39:03 +0800

    add views, serializers, urls to feature/transaction

--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from transaction.models import Transaction
+from transaction.serializers import TransactionSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    model = Transaction
+    serializer_class = TransactionSerializer
+
+
