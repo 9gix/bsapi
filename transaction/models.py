34d639f089a892c50d19aeb43501c6c53d5423d2
Yan Tianqi <yantq123@hotmail.com>
2014-07-12 18:24:30 +0800

    change design of transaction.borrower from user to membership

--- transaction/models.py
+++ transaction/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.conf import settings
 from communities.models import Membership
 
+
 class TransactionManager(models.Manager):
     def create_transaction(self, **kwargs):
         new_transaction = self.create(**kwargs)
-        new_transaction.book.current_holder = new_transaction.borrower
-        Membership.objects.get(user = new_transaction.book.owner).reputation.increase()
+        new_transaction.book.current_holder = new_transaction.borrower.user
+        new_transaction.book.owner.membership_set.get(community = new_transaction.borrower.community).reputation.increase()
         return new_transaction
 
 class Transaction(models.Model):
     book = models.ForeignKey('ownership.UserBook')
-    borrower = models.ForeignKey(settings.AUTH_USER_MODEL)
+    borrower = models.ForeignKey('communities.Membership')
     transaction_date = models.DateField(auto_now_add = True)
     ON_LOAN = 1
     AVAILABLE = 0
@@ -27,4 +28,4 @@ class Transaction(models.Model):
     
 
     def report_loss(self):
-        Membership.objects.get(user = self.borrower).reputation.decrease()
+        self.borrower.reputation.decrease()
