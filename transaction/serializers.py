2dd808268dd32bb904081e98aaf171568b10849e
Eugene <yeo.eugene.oey@gmail.com>
2014-07-20 20:55:56 +0800

    Remove transaction status

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -5,4 +5,4 @@ class TransactionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Transaction
-        fields = ('book', 'borrow_membership', 'transaction_date', 'transaction_status')
+        fields = ('book', 'borrow_membership', 'transaction_date')
