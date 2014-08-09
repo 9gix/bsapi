8a35c47cb0dbdbf2aa259269efa1e9fadb53fdb9
Eugene <yeo.eugene.oey@gmail.com>
2014-08-09 23:38:50 +0800

    Only the loan request owner can create a transaction

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,8 +1,27 @@
+from django.db.models import Q
+
 from rest_framework import serializers
+
 from transaction.models import Transaction
+from reservation.models import LoanRequest
 
 class TransactionSerializer(serializers.ModelSerializer):
 
+    url = serializers.HyperlinkedIdentityField(
+            view_name="transaction-detail",
+            lookup_field='pk')
+
     class Meta:
         model = Transaction
-        fields = ('loan_request', 'transaction_date')
+        fields = ('url', 'loan_request', 'transaction_date')
+
+    def get_fields(self, *args, **kwargs):
+        fields = super().get_fields(*args, **kwargs)
+        user = self.context['view'].request.user
+        if self.object:
+            qs = Q(transaction__isnull=True)|Q(pk=self.object.loan_request.pk)
+        else:
+            qs = Q(transaction__isnull=True)
+        loanrequests = LoanRequest.objects.filter(qs, owner_book__owner=user)
+        fields['loan_request'].queryset = loanrequests
+        return fields
--- transaction/views.py
+++ transaction/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from transaction.models import Transaction
 from transaction.serializers import TransactionSerializer
@@ -8,3 +9,4 @@ from transaction.serializers import TransactionSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
     model = Transaction
     serializer_class = TransactionSerializer
+    permission_classes = (permissions.IsAuthenticated, )
